24f761ab1e31ba67fe3a318a88c87df64a12a6b8
Mastermind730 <sourav.97032@gmail.com>
2023-11-13 18:42:04 +0530

    Added Tabs in about section

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,6 +1,56 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { useState,useTransition } from "react";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+    {
+      title: "Skills",
+      id: "skills",
+      content: (
+        <ul className="list-disc pl-2">
+          <li>Node.js</li>
+          <li>Express</li>
+          <li>Java</li>
+          <li>Python</li>
+          <li>JavaScript</li>
+          <li>React</li>
+        </ul>
+      ),
+    },
+    {
+      title: "Education",
+      id: "education",
+      content: (
+        <ul className="list-disc pl-2">
+          <li>Pimpri Chinchwad College of Engineering Pune</li>
+          <li>Kendriya Vidyala Army Area Pune</li>
+        </ul>
+      ),
+    },
+    {
+      title: "Certifications",
+      id: "certifications",
+      content: (
+        <ul className="list-disc pl-2">
+          <li>Certificate1</li>
+          <li>Certificate2</li>
+        </ul>
+      ),
+    },
+  ];
+
+
 const AboutSection = () => {
+    const [tab, setTab] = useState("skills");
+    const [isPending, startTransition] = useTransition();
+  
+    const handleTabChange = (id) => {
+      startTransition(() => {
+        setTab(id);
+      });
+    };
   return (
     <section>
       <div className=" grid md:grid-cols-2 lg:grid-cols-2 sm:grid-cols-1 p-2 m-12">
@@ -25,7 +75,32 @@ const AboutSection = () => {
             mollitia dolor. Repudiandae ea laborum veniam, ad corrupti
             laudantium.
           </p>
-          
+        <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
         </div>
       </div>
     </section>
